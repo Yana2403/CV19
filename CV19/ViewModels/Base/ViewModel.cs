13164d98b12c7410937d305c2d682d7ade751aa9
Яна Улендеева <ulendeeva.yana2013@yandex.ru>
2020-05-20 21:50:18 +0400

    Базовый класс модели представления

---
+++ CV19/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CV19.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged //интерфейс для изменений
+
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName=null)//можно не указывать переменную,в этом случае подставится имя метода, откуда идет вызов
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)  //для разрешения кольцевых обновлений свойств(когда одно за другим)
+        {
+            if (Equals(field, value)) return false; //если поле уже заполнено нужным, то ничего не делаем
+            field = value; //если нет,то обновляем
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
